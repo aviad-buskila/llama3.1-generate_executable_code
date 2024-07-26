fb3fc3be8ec90043d8db75117a58ca34c052f144
aviad-buskila <aviad810@gmail.com>
2024-07-26 21:48:44 +0300

    moved to ollama infra and updated requirements file and git ignore

--- demo.py
+++ demo.py
@@ -1,22 +1,10 @@
-import transformers
-import torch
+import ollama
 
-model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
-    device_map="auto",
-)
+response = ollama.chat(model='llama3.1:8b', messages=[
+    {
+        'role' : 'user',
+        'content' : 'Why is the sky blue?'
+    }])
 
-messages = [
-    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
-    {"role": "user", "content": "Who are you?"},
-]
-
-outputs = pipeline(
-    messages,
-    max_new_tokens=256,
-)
-print(outputs[0]["generated_text"][-1])
+print(response['message']['content'])
